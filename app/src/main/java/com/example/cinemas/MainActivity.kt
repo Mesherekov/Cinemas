abebe14ac08308f61481ce02076cfd053edc3826
Stalnodg <plof0686@gmail.com>
2025-07-19 17:59:39 +0600

    NavigationView 3

--- app/src/main/java/com/example/cinemas/MainActivity.kt
+++ app/src/main/java/com/example/cinemas/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.cinemas
 
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,12 +12,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -24,21 +32,48 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val fontFamily = FontFamily(
             Font(R.font.inter18, FontWeight.Normal)
         )
         setContent {
-            HomeScreen()
+            val navController = rememberNavController()
+            Scaffold(
+                bottomBar = {
+                    BottomNavBar(items = listOf(
+                        BottomNavItem(
+                            "Home",
+                            route = "home",
+                            icon = Icons.Default.Home
+                        ),
+                        BottomNavItem(
+                            "Profile",
+                            route = "profile",
+                            icon = Icons.Default.Person
+                        )
+                    ),
+                        navController = navController,
+                        onItemClick = {
+                            navController.navigate(it.route)
+                        }
+                        )
+                }
+            ) {
+                Navigation(navController = navController)
+            }
         }
     }
 
@@ -95,7 +130,7 @@ class MainActivity : ComponentActivity() {
         val backStack = navController.currentBackStackEntryAsState()
         NavigationBar(
             modifier = modifier,
-            containerColor = Color.Gray,
+            containerColor = Color(242f, 221f, 198f),
             tonalElevation = 5.dp
         ) {
             items.forEach { item ->
@@ -104,17 +139,25 @@ class MainActivity : ComponentActivity() {
                     onClick = { onItemClick(item) },
 
                     icon = {
-
+                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                            Icon(imageVector = item.icon, contentDescription = item.label)
+                            if (selected){
+                                Text(text = item.label,
+                                    textAlign = TextAlign.Center,
+                                    fontSize = 12.sp
+                                )
+                            }
+                        }
                     },
-                    colors = NavigationBarItemColors(
-                        selectedIconColor = Color.Blue,
-                        selectedTextColor = Color.Blue,
-                        selectedIndicatorColor = Color.Blue,
-                        Color.Gray,
-                        Color.Gray,
-                        Color.Gray,
-                        Color.Gray
-                    )
+//                    colors = NavigationBarItemColors(
+//                        selectedIconColor = Color.Blue,
+//                        selectedTextColor = Color.Blue,
+//                        selectedIndicatorColor = Color.Blue,
+//                        Color.Gray,
+//                        Color.Gray,
+//                        Color.Gray,
+//                        Color.Gray
+//                    )
                     )
             }
         }
