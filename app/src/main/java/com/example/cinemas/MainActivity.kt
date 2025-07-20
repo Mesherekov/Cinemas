88cbf9ae8f4e2de875ccb12da7edbdcd9a5ba769
Stalnodg <plof0686@gmail.com>
2025-07-20 20:13:58 +0600

    Profile Screen 1

--- app/src/main/java/com/example/cinemas/MainActivity.kt
+++ app/src/main/java/com/example/cinemas/MainActivity.kt
@@ -5,16 +5,24 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -175,9 +183,50 @@ class MainActivity : ComponentActivity() {
     }
     @Composable
     fun ProfileScreen() {
-        Box(modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center) {
-            Text(text = "profile")
+        PersonProfile()
+    }
+    @Composable
+    fun PersonProfile(){
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Column {
+                Box(modifier = Modifier.fillMaxWidth()
+                    .padding(10.dp),
+                    contentAlignment = Alignment.TopEnd) {
+                    Icon(
+                        modifier = Modifier
+                            .size(70.dp)
+                            .clickable {
+
+                            },
+                        imageVector = Icons.Default.Settings,
+                        contentDescription = "settings"
+                    )
+                }
+                Spacer(modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp))
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+
+                    Icon(
+                        imageVector = Icons.Default.AccountCircle,
+                        contentDescription = "person",
+                        modifier = Modifier.size(60.dp),
+                    )
+
+                    Spacer(Modifier.width(7.dp))
+                    Text(
+                        text = "Max",
+                        fontSize = 30.sp,
+                        maxLines = 2,
+                    )
+                }
+            }
         }
     }
 
