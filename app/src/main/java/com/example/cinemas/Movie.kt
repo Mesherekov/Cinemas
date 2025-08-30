69eab947dd1e095956a481467f26fcfc5916397e
Mesherekov <plof0676@gmail.com>
2025-08-30 21:08:04 +0600

    Parsing Movie

--- app/src/main/java/com/example/cinemas/Movie.kt
+++ app/src/main/java/com/example/cinemas/Movie.kt
@@ -1,6 +1,5 @@
 package com.example.cinemas
 
-import android.util.Log
 import androidx.compose.runtime.Composable
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -13,8 +12,7 @@ fun Movie(url: String){
     val movieInfo = MovieInfo(url)
     runBlocking {
         CoroutineScope(Dispatchers.IO).launch {
-//            movieInfo.parsing()
-//            Log.i("Info", movieInfo.movieData.rate)
+            movieInfo.parsing()
         }
     }
 }
--- app/src/main/java/com/example/cinemas/MovieInfo.kt
+++ app/src/main/java/com/example/cinemas/MovieInfo.kt
@@ -1,39 +1,52 @@
 package com.example.cinemas
 
 
+import android.util.Log
 import org.jsoup.Jsoup
 
 class MovieInfo(val url: String) {
     lateinit var movieData: MovieData
 
     fun parsing(){
-        val doc = Jsoup.connect(url).get()
-        var titles = doc.getElementsByAttributeValue("class", "picture picture-poster")
-        movieData.image = titles[0].child(0).attr("scrset")
-        titles = doc.getElementsByAttributeValue("class", "newFilmInfo_ticketsBtn button-large button-warning as-desktop")
-        movieData.tickets = Pair(titles[0].text(), titles[0].attr("href"))
-        titles = doc.getElementsByAttributeValue("class", "newFilmInfo_title")
-        movieData.name = titles[0].text()
-        titles = doc.getElementsByAttributeValue("class", "newFilmInfo_age")
-        movieData.age = titles[0].text()
-        titles = doc.getElementsByAttributeValue("class", "newFilmInfo_genreMenu swipe outer-mobile inner-mobile")
-        val listTag = mutableListOf<String>()
-        titles[0].forEach {
-            listTag.add(it.text())
+        val error = runCatching {
+            movieData = MovieData()
+            val doc = Jsoup.connect(url).get()
+            var titles = doc.getElementsByAttributeValue("class", "picture picture-poster")
+            movieData.image = titles[0].child(0).attr("srcset")
+            titles = doc.getElementsByAttributeValue(
+                "class",
+                "newFilmInfo_ticketsBtn button-large button-warning as-desktop"
+            )
+            movieData.tickets = Pair(titles[0].text(), titles[0].attr("href"))
+            titles = doc.getElementsByAttributeValue("class", "newFilmInfo_title")
+            movieData.name = titles[0].text()
+            titles = doc.getElementsByAttributeValue("class", "newFilmInfo_age")
+            movieData.age = titles[0].text()
+            titles = doc.getElementsByAttributeValue(
+               "class",
+                "newFilmInfo_genreMenu swipe outer-mobile inner-mobile"
+            )
+            val listTag = mutableListOf<String>()
+            titles[0].forEach {
+                listTag.add(it.text())
+            }
+            movieData.tags = listTag
+            titles = doc.getElementsByAttributeValue("class", "visualEditorInsertion visualEditorInsertion-info more_content")
+            movieData.filminfo = titles[0].text()
+            titles = doc.getElementsByAttributeValue("class", "ratingBlockCard_local")
+            movieData.rate = titles[0].text()
         }
-        movieData.tags = listTag
-        titles = doc.getElementsByAttributeValue("class", "styles_paragraph__wEGPz")
-        movieData.filminfo = titles[0].text()
-        titles = doc.getElementsByAttributeValue("class", "ratingBlockCard_local")
-        movieData.rate = titles[0].text()
+        error.onFailure {
+            Log.e("MovieError", it.toString())
+        }
     }
 }
 data class MovieData(
-    var name: String,
-    var image: String,
-    var filminfo: String,
-    var rate: String,
-    var tickets: Pair<String, String>,
-    var tags: List<String>,
-    var age: String
+    var name: String = "",
+    var image: String = "",
+    var filminfo: String = "",
+    var rate: String = "",
+    var tickets: Pair<String, String> = Pair("", ""),
+    var tags: List<String> = arrayListOf(""),
+    var age: String  = ""
 )
